4a6e87581db2b7ad62b14e8d63cf48add046c7da
Bimal Kumal <140985679+bimal1023@users.noreply.github.com>
2025-02-24 02:40:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 import numpy as np
-
+with open("model.pkl", "rb") as file:
+    model = pickle.load(file)
 st.title("My ML Model Web App")
 st.write("Provide the input features to get a prediction.")
 
